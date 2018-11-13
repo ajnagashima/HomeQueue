6846eb7ac39ff9c08ea6195a308ce136ffb1a087
AJ Nagashima <arthurjnagashima@gmail.com>
2018-11-13 14:26:53 -0500

    Add spotify Auth Page
    Todo:
    Capture token from URL

--- Application/Login.js
+++ Application/Login.js
@@ -6,34 +6,72 @@ import {View,
   KeyboardAvoidingView,
   TextInput,
   Image,
+  WebView,
+  Modal,
+  Alert
 } from 'react-native'
 import LoginForm from './LoginForm'
+
+
+const redirectURI = 'https://localhost:8888'
+scope = 'user-read-email'
+client_id = 'b610b0bf7b7644fab5905b20ad6f03e1'
 
 export default class Login extends Component{
   constructor(props){
     super(props)
+    this.state = {
+        view: null,
+        show: false,
+    }
   }
 
   loginPress(username, password){
-    console.log(password)
-    console.log(username)
-    if(password !== '' && username !== '')
-      this.props.navigation.navigate('Main')
+    var scopes = 'user-read-email'
+    var url = 'https://accounts.spotify.com/authorize'
+    url += '?response_type=token'
+    url += '&client_id='+encodeURIComponent(client_id)
+    url += '&scope='+encodeURIComponent(scopes)
+    url += '&redirect_uri='+encodeURIComponent(redirectURI)
+    this.setState({
+        view: url,
+        show: true,
+    })
+
+      }
+
+  loggedin(url){
+    console.log(url)
+    
+    this.props.navigation.navigate('Main')
   }
 
   render(){
     return(
-        <KeyboardAvoidingView behavior="padding" style={styles.container}>
-          <View style={styles.logoContainer}>
-            <Image
-              resizeMode="contain"
-              style={styles.logoContainer}
-              source={require('../Resources/logo.png')}/>
-          </View>
-          <View style={styles.loginContainer}>
-            <LoginForm onPress={this.loginPress.bind(this)}/>
-          </View>
-        </KeyboardAvoidingView>
+            <KeyboardAvoidingView behavior="padding" style={styles.container}>
+                {
+                    this.state.show && 
+                    <Modal
+                    visible ={this.state.show}
+                    animationtype = "slide"
+                    transparent ={false}
+                    onRequestClose={() => {this.setState({view: null,show:false,})}}
+                    >
+                        <WebView
+                            source={{uri:this.state.view}}
+                        />
+                    </Modal>
+                }
+                <View style={styles.logoContainer}>
+                    <Image
+                    resizeMode="contain"
+                    style={styles.logoContainer}
+                    source={require('../Resources/logo.png')}/>
+                </View>
+                <View style={styles.loginContainer}>
+                    <LoginForm onPress={this.loginPress.bind(this)}/>
+                </View>
+            </KeyboardAvoidingView>
     );
   }
 }
@@ -52,4 +90,5 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     flex:1
   }
+
 });
